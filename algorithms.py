ae2427fffdd5416d95fd928665c58e08271c0e6c
MrResor <jakusienko@gmail.com>
2023-04-24 11:09:23 +0200

    Added docstrings for Ant class.

--- algorithms.py
+++ algorithms.py
@@ -8,10 +8,52 @@ class Genetic:
 
 
 class Ant:
+    """ Class of Ant Colony Algorithm. Holds all the variables and functions
+        needed for performing said algorihtm.\n
+
+        Attributes:\n
+        tours           -- Number of tours the program will simulate before
+        outputing the best found result.\n
+        alpha           -- Parameter infulencing impact of the pheromone on
+        the ant decision making.\n
+        beta            -- Parameter influencing impact of the distance
+        between the nodes on the ant decision making.\n
+        rho             -- Evporation rate of the pheromones.\n
+        d               -- Distance between nodes.\n
+        size            -- Number of nodes.\n
+        tau             -- Pheromones for each edge between nodes.\n
+        n               -- Inverse of distance between nodes.\n
+        ants            -- Table with currently simulated ants.\n
+        a               -- Table with decision making variables for each edge
+        between nodes.\n
+        start           -- Starting cities of simulated ants.
+        p               -- Probabilities of choosing given node for simulated
+        ant.\n
+
+        Methods:\n
+        run             -- Runs the prepaired algorithm with the parameters
+        set in __init__ and with the distance matrix passed.\n
+        ants_tables     -- Creates self.tau, self.n and best tables that are
+        used later in the algorithm.\n
+        ants_and_a      -- Creates self.ants, self.a and self.start for each
+        tour.\n
+        ants_traveling  -- Simulates the traveling process of ants along with
+        decision making.\n
+        pheromones      -- Simulates the evaporation of pheromones in self.tau.
+    """
+
     def __init__(self, params: dict) -> None:
+        """ Initialization of Ant class, takes dict as parameter and assigns
+            values from it into corresponding variables.
+        """
+
         self.tours, self.alpha, self.beta, self.rho = params.values()
 
-    def run(self, d, size) -> None:
+    def run(self, d: np.ndarray, size: int) -> None:
+        """ Runs the algorithm. Takes distance matrix and number of nodes as
+            parameters using the parameters set in __init__ performs a
+            simulation of Ant Colony finding shortest path.
+        """
         start = time.perf_counter()
         logging.info("Staring algorithm",
                      extra={'runtime': time.perf_counter() - start})
@@ -22,68 +64,72 @@ class Ant:
             logging.info('Trip %s',
                          str(trip),
                          extra={'runtime': time.perf_counter() - start})
-            self.ant_and_a()
+            self.ants_and_a()
             self.ants_traveling()
-            index = np.argmin(self.ant[:, self.size])
-            if best[self.size] > self.ant[index][self.size]:
-                best = self.ant[index]
+            index = np.argmin(self.ants[:, self.size])
+            best = self.ants[index] if best[self.size] > self.ants[index][
+                self.size] else best
             self.pheromones()
         logging.info('Finished.',
                      extra={'runtime': time.perf_counter() - start})
         logging.info('Best Distance: %s',
                      str(best[self.size]),
-                     extra={'runtime': None})
+                     extra={'runtime': 0})
 
     def ants_tables(self) -> np.ndarray:
-        # fucntion for creating necessary tables
+        """ Creates necessary tables for the algorithm to run. These tables
+            need to be created just once.
+        """
+
         self.tau = np.full((self.size, self.size), 1/self.d.max())
         self.n = 1/self.d
         np.fill_diagonal(self.n, 0)
-        best = np.full(self.size + 1, 0.0)
+        best = np.arange(self.size + 1, dtype='object')
         best[self.size] = np.Inf
         return best
 
-    def ant_and_a(self) -> None:
-        # creating anew ant table and filling a
-        self.ant = np.full((self.size, self.size + 1),
-                           self.size + 1, dtype="object")
-        self.ant[:, self.size] = float(0)
+    def ants_and_a(self) -> None:
+        """ Creates necessary tables for the algorithm to run. These tables
+            are created anew for each tour.
+        """
+
+        self.ants = np.full((self.size, self.size + 1),
+                            self.size + 1, dtype="object")
+        self.ants[:, self.size] = float(0)
         self.a = self.tau ** self.alpha * self.n ** self.beta
         self.start = np.random.randint(0, self.size, self.size)
         self.a = [val / sum(val) for val in self.a]
 
     def ants_traveling(self) -> None:
-        # function that simulates "travelling" of ants
-        self.ant[:, 0] = self.start
+        """ Simulates the behaviour and the traveling of ants in search of the
+            optimal path.
+        """
+        self.ants[:, 0] = self.start
         for i in range(self.size):
             for j in range(1, self.size):
-                self.p = [self.a[self.ant[i, j-1]][q] *
-                          (q not in self.ant[i]) for q in range(self.size)]
+                self.p = [self.a[self.ants[i, j-1]][q] *
+                          (q not in self.ants[i]) for q in range(self.size)]
                 self.p = [val / sum(self.p) for val in self.p]
-                r = 0
-                while r == 0:
-                    r = np.random.rand(1)
-                for q in range(self.size):
-                    r -= self.p[q]
-                    if r <= 0:
-                        self.ant[i][j] = q
-                        self.ant[i][self.size] += self.d[
-                            self.ant[i][j - 1]][self.ant[i][j]]
-                        break
-            self.ant[i][self.size] += self.d[
-                self.ant[i][self.size-1]][self.ant[i][0]]
+                q = np.argmax(np.random.multinomial(1, self.p, size=1))
+                self.ants[i][j] = q
+                self.ants[i][self.size] += self.d[
+                    self.ants[i][j - 1]][self.ants[i][j]]
+            self.ants[i][self.size] += self.d[
+                self.ants[i][self.size-1]][self.ants[i][0]]
 
     def pheromones(self) -> None:
-        # function for updating pheromones on each arc
+        """ Updates the ammount of pheromone on each edge represented by tau.
+        """
+
         self.tau *= (1-self.rho)
         # TODO check if np.roll can do this faster
         for i in range(self.size):
             for j in range(1, self.size):
-                self.tau[self.ant[i][j - 1]][
-                    self.ant[i][j]] += 1/self.ant[i][self.size]
-                self.tau[self.ant[i][j]][
-                    self.ant[i][j - 1]] += 1/self.ant[i][self.size]
-            self.tau[self.ant[i][0]][
-                self.ant[i][self.size - 1]] += 1/self.ant[i][self.size]
-            self.tau[self.ant[i][self.size - 1]][
-                self.ant[i][0]] += 1/self.ant[i][self.size]
+                self.tau[self.ants[i][j - 1]][
+                    self.ants[i][j]] += 1/self.ants[i][self.size]
+                self.tau[self.ants[i][j]][
+                    self.ants[i][j - 1]] += 1/self.ants[i][self.size]
+            self.tau[self.ants[i][0]][
+                self.ants[i][self.size - 1]] += 1/self.ants[i][self.size]
+            self.tau[self.ants[i][self.size - 1]][
+                self.ants[i][0]] += 1/self.ants[i][self.size]
