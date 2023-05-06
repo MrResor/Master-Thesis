477a666f2fcc6a7027ddf95760b5c96c983c5cd6
Robert Kwiatkowski <jakusienko@gmail.com>
2023-05-06 22:30:59 +0200

    Checked up on docstrings and formatting of ant colony algorithm.

--- algorithms.py
+++ algorithms.py
@@ -157,16 +157,15 @@ class Ant:
         needed for performing said algorihtm.\n
 
         Attributes:\n
-        tours           -- Number of tours the program will simulate before
-        outputing the best found result.\n
-        alpha           -- Parameter infulencing impact of the pheromone on
-        the ant decision making.\n
-        beta            -- Parameter influencing impact of the distance
-        between the nodes on the ant decision making.\n
-        rho             -- Evporation rate of the pheromones.\n
+        tours           -- Number of tours simulated by the program.\n
+        alpha           -- Impact of the pheromone on the ant decision making.
+        \n
+        beta            -- Impact of the distance between the nodes on the ant
+        decision making.\n
+        rho             -- Evaporation rate of the pheromone.\n
         d               -- Distance between nodes.\n
         size            -- Number of nodes.\n
-        tau             -- Pheromones for each edge between nodes.\n
+        tau             -- Table of pheromone for each edge between nodes.\n
         n               -- Inverse of distance between nodes.\n
         ants            -- Table with currently simulated ants.\n
         a               -- Table with decision making variables for each edge
@@ -175,19 +174,19 @@ class Ant:
 
         Methods:\n
         run             -- Runs the prepaired algorithm with the parameters
-        set in __init__ and with the distance matrix passed.\n
-        ants_tables     -- Creates self.tau, self.n and best tables that are
-        used later in the algorithm.\n
-        ants_and_a      -- Creates self.ants, self.a and self.start for each
-        tour.\n
+        parameters set in __init__ and with the distance matrix and
+        information about number of nodes passed.\n
+        ants_tables     -- Creates tau, n and best tables that are used in the
+        algorithm.\n
+        ants_and_a      -- Creates ants, a and start for each tour.\n
         ants_traveling  -- Simulates the traveling process of ants along with
         decision making.\n
-        pheromones      -- Simulates the evaporation of pheromones in self.tau.
+        pheromones      -- Simulates the evaporation of pheromone in tau.
     """
 
     def __init__(self, params: dict) -> None:
-        """ Initialization of Ant class, takes dict as parameter and assigns
-            values from it into corresponding variables.
+        """ Initialization of Ant class, takes dictionary as parameter and
+            assigns values from it into corresponding variables.
         """
 
         self.tours, self.alpha, self.beta, self.rho = params.values()
@@ -199,26 +198,34 @@ class Ant:
         """
 
         start = perf_counter()
-        logging.info("Staring algorithm",
-                     extra={'runtime': perf_counter() - start})
+        logging.info(
+            "Staring algorithm",
+            extra={'runtime': perf_counter() - start}
+        )
         self.d = d
         self.size = size
         best = self.ants_tables()
         for trip in range(self.tours):
-            logging.info('Trip %s',
-                         str(trip),
-                         extra={'runtime': perf_counter() - start})
+            logging.info(
+                'Trip %s',
+                str(trip),
+                extra={'runtime': perf_counter() - start}
+            )
             self.ants_and_a()
             self.ants_traveling()
             index = np.argmin(self.ants[:, -1])
             best = self.ants[index] if best[-1] > self.ants[index][
                 -1] else best
             self.pheromones()
-        logging.info('Finished.',
-                     extra={'runtime': perf_counter() - start})
-        logging.info('Best Distance: %s',
-                     str(best[-1]),
-                     extra={'runtime': 0})
+        logging.info(
+            'Finished.',
+            extra={'runtime': perf_counter() - start}
+        )
+        logging.info(
+            'Best Distance: %s',
+            str(best[-1]),
+            extra={'runtime': 0}
+        )
 
     def ants_tables(self) -> np.ndarray:
         """ Creates necessary tables for the algorithm to run. These tables
@@ -229,7 +236,7 @@ class Ant:
         self.n = 1/self.d
         np.fill_diagonal(self.n, 0)
         best = np.arange(self.size + 1, dtype='object')
-        best[self.size] = np.Inf
+        best[-1] = np.Inf
         return best
 
     def ants_and_a(self) -> None:
@@ -237,9 +244,12 @@ class Ant:
             are created anew for each tour.
         """
 
-        self.ants = np.full((self.size, self.size + 1),
-                            self.size + 1, dtype="object")
-        self.ants[:, self.size] = float(0)
+        self.ants = np.full(
+            (self.size, self.size + 1),
+            self.size + 1,
+            dtype="object"
+        )
+        self.ants[:, -1] = float(0)
         self.a = self.tau ** self.alpha * self.n ** self.beta
         self.start = np.random.randint(0, self.size, self.size, dtype='int32')
         self.a /= self.a.sum(axis=1, keepdims=True)
@@ -252,12 +262,14 @@ class Ant:
         self.ants[:, 0] = self.start
         for ant in self.ants:
             for j in range(1, self.size):
-                p = [self.a[ant[j-1], q] * (q not in ant[0:-1])
-                     for q in range(self.size)]
+                p = [
+                    self.a[ant[j-1], q] * (q not in ant[0:-1])
+                    for q in range(self.size)
+                ]
                 p /= np.sum(p)
                 q = np.argmax(np.random.multinomial(1, p, size=1))
                 ant[j] = int(q)
-            ant[self.size] = self.d[
+            ant[-1] = self.d[
                 ant[:-1].astype('int32'),
                 np.roll(ant[:-1], -1).astype('int32')
             ].sum()
@@ -268,10 +280,9 @@ class Ant:
 
         self.tau *= (1-self.rho)
         for ant in self.ants:
-            self.tau[ant[0:-1].astype('int32'), np.roll(ant[
-                0:-1], -1).astype('int32')] += 1/ant[self.size]
-            self.tau[ant[0:-1].astype('int32'), np.roll(ant[
-                0:-1], 1).astype('int32')] += 1/ant[self.size]
+            a = ant[0: -1].astype('int32')
+            self.tau[a, np.roll(a, -1)] += 1/ant[-1]
+            self.tau[a, np.roll(a, 1)] += 1/ant[-1]
 
 
 class smallest_edge_algorithm:
